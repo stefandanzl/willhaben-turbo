4f4e7b331bb1c27c46028bd5ea3b8527ca42d640
stefan.danzl <stefan.danzl@ksengineers.com>
2024-04-16 18:09:22 +0200

    sec

--- mapserver.js
+++ mapserver.js
@@ -1,23 +1,30 @@
 const express = require('express');
-const cors = require('cors'); // Import the cors middleware
+const cors = require('cors');
+const willhaben = require('willhaben')
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Use cors middleware
 app.use(cors());
-
 app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+let data = {};
+
+willhaben.getListings('https://www.willhaben.at/iad/kaufen-und-verkaufen/marktplatz/pc-komponenten/-5882?keyword=rtx&rows=1000').then(json => {
+    console.log(json)
+    data = json;
+})
+
+
 app.get('/coordinates', (req, res) => {
   // You would fetch your coordinates data from a database or another source
-  const coordinates = [
-    { lat: 51.505, lng: -0.09 },
-    { lat: 51.51, lng: -0.1 },
-    // Add more coordinates as needed
-  ];
-  res.json(coordinates);
+//   const coordinates = [
+//     { id: 1, lat: 51.505, lng: -0.09, name: 'Location 1', description: 'This is location 1' },
+//     { id: 2, lat: 51.51, lng: -0.1, name: 'Location 2', description: 'This is location 2' },
+//     // Add more coordinates with additional information as needed
+//   ];
+  res.json(data);
 });
